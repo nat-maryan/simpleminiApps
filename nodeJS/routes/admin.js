da7c9f88dcd640c2eb59eb5e5e08f1c2c500910b
Natalia Margianou <natalia.margianou@upstreamsystems.com>
2020-01-24 17:50:16 +0200

    UPD: Use helper function for path routing

--- nodeJS/routes/admin.js
+++ nodeJS/routes/admin.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
 
+const routeDir = require('../helpers/path');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(routeDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next)=>{
