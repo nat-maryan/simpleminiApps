f6b16a57df8dbb39f72f14d74791c772e2ce43dd
Natalia Margianou <natalia.margianou@upstreamsystems.com>
2020-01-24 17:17:36 +0200

    ADD: Express Routing and Path filtering

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-  
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-   
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add product</button></form>')
-});
-
-app.use('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  
-    res.send('<h1>Lalalallalala</h1>')
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
 });
 
 app.listen(3002);
