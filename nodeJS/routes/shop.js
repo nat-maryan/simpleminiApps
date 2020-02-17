50272bc8fe66e913c96b894d85ba4660f220dbff
Natalia Margianou <natalia.margianou@upstreamsystems.com>
2020-02-17 17:28:54 +0200

    Templating with Pug

--- nodeJS/routes/shop.js
+++ nodeJS/routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const routeDir = require('../helpers/path');
+const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(routeDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
