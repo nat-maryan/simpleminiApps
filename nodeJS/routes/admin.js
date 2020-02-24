042a3b3fa478c108d2c584608dae9ff31ecd000f
Natalia Margianou <natalia.margianou@upstreamsystems.com>
2020-02-24 12:32:17 +0200

    FInishing templating with pug

--- nodeJS/routes/admin.js
+++ nodeJS/routes/admin.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product');
+    res.render('add-product', { path: '/admin/add-product', pageTitle: 'Add Product' });
 });
 
 router.post('/add-product', (req, res, next) => {
