bd66a9004760a4f807a285b605709d28d148ee49
Natalia Margianou <natalia.margianou@upstreamsystems.com>
2020-02-24 15:57:34 +0200

    Update to ejs templating

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('handlebars', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' }));
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' }));
+//app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
